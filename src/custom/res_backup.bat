8c5027580b37e89e7f1c238bc8bc994eaf3bff35
CarlosCalderon01 <Carlos.Calderon01@Outlook.com>
2024-08-16 20:07:04 -0500

    restaurar backup

--- src/custom/res_backup.bat
+++ src/custom/res_backup.bat
@@ -10,9 +10,12 @@ REM Ruta de la copia de seguridad
 set BACKUP_PATH=src\backup\arcee_backup.sql
 
 REM Crear la base de datos si no existe
+REM Default_Paht: C:\Program Files\PostgreSQL\15\bin\psql.exe"
+REM special_Paht: E:\PostgreSQL\16\bin\pg_dump.exe
 echo Creando la base de datos %DATABASE% si no existe...
 set PGPASSWORD=%PASSWORD%
-"E:\PostgreSQL\16\bin\psql.exe" -h %SERVER% -p %PORT% -U %USERNAME% -c "CREATE DATABASE %DATABASE%"
+"C:\Program Files\PostgreSQL\15\bin\psql.exe" -h %SERVER% -p %PORT% -U %USERNAME% -c "CREATE DATABASE %DATABASE%"
+
 
 if %ERRORLEVEL% equ 0 (
     echo Base de datos %DATABASE% creada exitosamente.
@@ -22,7 +25,7 @@ if %ERRORLEVEL% equ 0 (
 
 REM Restaurar la base de datos desde la copia de seguridad
 echo Restaurando la base de datos desde %BACKUP_PATH%...
-"E:\PostgreSQL\16\bin\psql.exe" -h %SERVER% -p %PORT% -U %USERNAME% -d %DATABASE% -f %BACKUP_PATH%
+"C:\Program Files\PostgreSQL\15\bin\psql.exe" -h %SERVER% -p %PORT% -U %USERNAME% -d %DATABASE% -f %BACKUP_PATH%
 
 if %ERRORLEVEL% equ 0 (
     echo Base de datos restaurada exitosamente.
